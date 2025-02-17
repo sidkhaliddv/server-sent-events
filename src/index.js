b21c71fca83cc24b419d55e4d666da577a08601d
sidkhaliddv <sidkhaliddv@gmail.com>
2025-02-17 23:33:16 +0500

    added /events

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res)=>{
   res.send('Hello server events')
 })
 
+app.get('/events', (req, res)=>{
+  res.setHeader('Content-Type', 'text/event-stream');
+  
+  sendMessageIntervals(res)
+})
+
 app.listen('9090', ()=>{
   console.log('server is listening on 9090')
 })
+
+let i=0;
+const sendMessageIntervals = (res) => {
+  setTimeout(()=>{
+    res.write(`data: you got it ${i++} \n\n`)
+    sendMessageIntervals(res)
+  }, 1000)
+}
